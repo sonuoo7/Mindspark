a7f87992a755acd4af2dbf780b8b2d53a00492d6
Anshul Vats <avanshul0001@gmail.com>
2023-03-23 19:11:45 +0530

    method for user registration added

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { auth, db } from "../../firebase-config";
+import { db } from "../../firebase-config";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function Registration() {
   const [firstName, setFirstName] = useState("");
@@ -7,27 +9,39 @@ function Registration() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const auth = getAuth();
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
 
-        db.collection("users").doc(user.uid).set({
-          firstName,
-          lastName,
-          email,
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    // auth
+    //   .createUserWithEmailAndPassword(email, password)
+    //   .then((userCredential) => {
+    //     const user = userCredential.user;
+
+    //     db.collection("users").doc(user.uid).set({
+    //       firstName,
+    //       lastName,
+    //       email,
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <label>
         First Name:
         <input
@@ -60,7 +74,7 @@ function Registration() {
           onChange={(e) => setPassword(e.target.value)}
         />
       </label>
-      <button type="submit">Register</button>
+      <button type="submit" onClick={handleSubmit}>Register</button>
     </form>
   );
 }
